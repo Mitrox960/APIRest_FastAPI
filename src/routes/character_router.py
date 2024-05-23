91012e50e1cfa7a860a62ad78ef3588e277c90cf
Mitrox960 <113670212+Mitrox960@users.noreply.github.com>
2024-05-23 11:20:00 +0200

    Character (#2)
    Get character
    Delete Character
    Update Character

--- src/routes/character_router.py
+++ src/routes/character_router.py
@@ -1,12 +1,23 @@
-from fastapi import APIRouter
-from src.controllers import character_controller
-from src.models.charactersModel import CharacterCreate, Characters
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from src.controllers import character_controller
+from src.models.charactersModel import CharacterCreate, Characters
 from src.database import get_session
 
 router = APIRouter()
 
 @router.post("/create_character", response_model=Characters)
-def create_account(character_create: CharacterCreate, db: Session = Depends(get_session)):
+def create_character(character_create: CharacterCreate, db: Session = Depends(get_session)):
     return character_controller.create_character(character_create, db)
+
+@router.get("/get_character/{character_id}", response_model=Characters)
+def get_character(character_id: int, db: Session = Depends(get_session)):
+    return character_controller.get_character(character_id, db)
+
+@router.delete("/delete_character/{character_id}")
+def delete_character(character_id: int, db: Session = Depends(get_session)):
+    return character_controller.delete_character(character_id, db)
+
+@router.put("/update_character_name/{character_id}")
+def update_character_name(character_id: int, new_name: str, db: Session = Depends(get_session)):
+    return character_controller.update_character_name(character_id, new_name, db)
--- src/services/character_service.py
+++ src/services/character_service.py
@@ -5,10 +5,33 @@ class CharactersService:
     @staticmethod
     def create_character(db: Session, name: str, gender: str, ethnic: str) -> Characters:
         # Logique métier
-            # Pas de @ dupliqué
-            # Password avec x catactères min...
+        # Pas de @ dupliqué
+        # Password avec x caractères min...
         character = Characters(name=name, gender=gender, ethnic=ethnic)
         db.add(character)
         db.commit()
         db.refresh(character)
         return character
+
+    @staticmethod
+    def get_character(db: Session, character_id: int) -> Characters:
+        character = db.get(Characters, character_id)
+        return character
+
+    @staticmethod
+    def delete_character(db: Session, character_id: int) -> Characters:
+        character = db.get(Characters, character_id)
+        if character:
+            db.delete(character)
+            db.commit()
+            
+    @staticmethod
+    def update_character_name(db: Session, character_id: int, new_name: str) -> Characters:
+        character = db.get(Characters, character_id)
+        if character:
+            character.name = new_name
+            db.commit()
+            db.refresh(character)
+            return character
+
+ 
